39796974137fed25061479cb68b7c8b94625fefe
Zdenek K <zde.kod@seznam.cz>
2021-10-24 22:40:20 +0200

    Resolved #4

--- PhishReport/Properties/AssemblyInfo.cs
+++ PhishReport/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PhishReport")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Zdenek K. 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 
--- PhishReport/RibbonSecurity.cs
+++ PhishReport/RibbonSecurity.cs
@@ -14,89 +14,104 @@ namespace PhishReport
 
 		private void btnReport_Click(object sender, RibbonControlEventArgs e)
 		{
+			//Application.ProductName returns "Outlook"
+			const string APP_NAME = "PhishReport";
 			bool supported = false;
 			Inspector inspector = e.Control.Context as Inspector;
 			if (inspector != null)
 			{
 				MailItem originalMail = inspector.CurrentItem as MailItem;
-				Send(originalMail);
-				supported = true;
+				try
+				{
+					Send(originalMail);
+					supported = true;
+				}
+				catch (System.Exception exc)
+				{
+					MessageBox.Show(exc.Message);
+				}
 			}
 			else
-			{ 
+			{
 				Explorer explorer = new Outlook.Application().ActiveExplorer() as Explorer;
 				Selection selection = explorer.Selection;
 
-				if(selection.Count == 0)
+				if (selection.Count == 0)
 				{
-					MessageBox.Show(Properties.Settings.Default.NoMailSelected);
+					string s = Properties.Settings.Default.NoMailSelected;
+					MessageBox.Show(s, APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Error);
 					return;
 				}
 
-				foreach(var selectedItem in selection)
+				foreach (var selectedItem in selection)
 				{
-					if(selectedItem is MailItem)
+					if (selectedItem is MailItem)
 					{
-						Send((MailItem)selectedItem);
-						supported = true;
+						try
+						{
+							Send((MailItem)selectedItem);
+							supported = true;
+						}
+						catch (System.Exception exc)
+						{
+							string s = exc.Message;
+							MessageBox.Show(s, APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Error);
+						}
 					}
 				}
 			}
 
-			if(!supported)
+			if (!supported)
 			{
-				MessageBox.Show(Properties.Settings.Default.NoSupportedMailSelected);
+				string s = Properties.Settings.Default.NoSupportedMailSelected;
+				MessageBox.Show(s, APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Error);
 				return;
+			}
+			else
+			{
+				string s = Properties.Settings.Default.Thanks;
+				MessageBox.Show(s, APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Information);
 			}
 		}
 
 		void Send(MailItem originalMail)
-		{ 
+		{
 			if (originalMail != null)
 			{
-				try
+				string propName = "http://schemas.microsoft.com/mapi/proptag/0x007D001E";
+				var propAccessor = originalMail.PropertyAccessor;
+				string header = propAccessor.GetProperty(propName);
+
+				MailItem fwMail = (MailItem)Globals.ThisAddIn.Application.CreateItem(OlItemType.olMailItem);
+				fwMail.To = Properties.Settings.Default.FwdAddress;
+				fwMail.Subject = string.Format("{0}: {1}", Properties.Settings.Default.Subject, originalMail.Subject);
+				fwMail.Body = header;
+				fwMail.Attachments.Add(originalMail);
+				fwMail.Save();
+				fwMail.Send();
+
+				OlDefaultFolders targetFolder = OlDefaultFolders.olFolderInbox;
+
+				int targetFolderId = (int)Properties.Settings.Default.TargetFolder;
+				switch (targetFolderId)
 				{
-					string propName = "http://schemas.microsoft.com/mapi/proptag/0x007D001E";
-					var propAccessor = originalMail.PropertyAccessor;
-					string header = propAccessor.GetProperty(propName);
-
-					MailItem fwMail = (MailItem)Globals.ThisAddIn.Application.CreateItem(OlItemType.olMailItem);
-					fwMail.To = Properties.Settings.Default.FwdAddress;
-					fwMail.Subject = string.Format("{0}: {1}", Properties.Settings.Default.Subject, originalMail.Subject);
-					fwMail.Body = header;
-					fwMail.Attachments.Add(originalMail);
-					fwMail.Save();
-					fwMail.Send();
-
-					OlDefaultFolders targetFolder = OlDefaultFolders.olFolderInbox;
-
-					int targetFolderId = (int)Properties.Settings.Default.TargetFolder;
-					switch (targetFolderId)
-					{
-						case 1:
-							targetFolder = OlDefaultFolders.olFolderJunk;
-							break;
-						case 2:
-							targetFolder = OlDefaultFolders.olFolderDeletedItems;
-							break;
-						default:
-							targetFolderId = 0;
-							break;
-					}
-
-					if (targetFolderId > 0)
-					{
-						originalMail.Close(OlInspectorClose.olDiscard);
-						Outlook.Application a = new Outlook.Application();
-						MAPIFolder target = a.ActiveExplorer().Session.GetDefaultFolder(targetFolder);
-						originalMail.Move(target);
-					}
-
-					MessageBox.Show(Properties.Settings.Default.Thanks);
+					case 1:
+						targetFolder = OlDefaultFolders.olFolderJunk;
+						break;
+					case 2:
+						targetFolder = OlDefaultFolders.olFolderDeletedItems;
+						break;
+					default:
+						targetFolderId = 0;
+						break;
 				}
-				catch (System.Exception exc)
+
+				if (targetFolderId > 0)
 				{
-					System.Windows.Forms.MessageBox.Show(exc.Message);
+					originalMail.Close(OlInspectorClose.olDiscard);
+					Outlook.Application a = new Outlook.Application();
+					MAPIFolder target = a.ActiveExplorer().Session.GetDefaultFolder(targetFolder);
+					originalMail.Move(target);
 				}
 			}
 		}
